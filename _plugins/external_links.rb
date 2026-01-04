72f7a0aa3ed9caa21a86215a1ea5d3ed05093c9e
Pete <pete@dugg.in>
2026-01-03 20:40:59 -0700

    testing different way to handle videos

--- _plugins/external_links.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  module ExternalLinks
-    def external_links(content)
-      content.gsub(/<a([^>]*)href="(https?:\/\/[^"]+)"([^>]*)>/, '<a\1href="\2" target="_blank"\3>')
-    end
-  end
-end
-
-Liquid::Template.register_filter(Jekyll::ExternalLinks)
